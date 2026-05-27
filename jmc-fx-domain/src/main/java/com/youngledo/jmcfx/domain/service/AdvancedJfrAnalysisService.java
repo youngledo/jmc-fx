acc1e93a07e034ed8623d9ba53fb6ec467c9680d
Huang Xiao <youngledo@qq.com>
2026-05-28 01:08:04 +0800

    fix: require advanced memory analysis implementations
    (cherry picked from commit f564828739f6cf9259b1b766a8dd104df0f7aae5)

--- jmc-fx-domain/src/main/java/com/youngledo/jmcfx/domain/service/AdvancedJfrAnalysisService.java
+++ jmc-fx-domain/src/main/java/com/youngledo/jmcfx/domain/service/AdvancedJfrAnalysisService.java
@@ -1,7 +1,5 @@
 package com.youngledo.jmcfx.domain.service;
 
-import java.util.List;
-
 import com.youngledo.jmcfx.domain.model.EventHeatmap;
 import com.youngledo.jmcfx.domain.model.MemoryAnalysisReport;
 import com.youngledo.jmcfx.domain.model.RecordingSummary;
@@ -10,7 +8,5 @@ public interface AdvancedJfrAnalysisService {
 
     EventHeatmap loadEventHeatmap(RecordingSummary recording, int bucketCount, int maxEventTypes);
 
-    default MemoryAnalysisReport loadMemoryAnalysis(RecordingSummary recording, int maxIssues) {
-        return new MemoryAnalysisReport(0, 0, List.of());
-    }
+    MemoryAnalysisReport loadMemoryAnalysis(RecordingSummary recording, int maxIssues);
 }
